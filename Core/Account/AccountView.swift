a785039cfeeeced436ecb368880a0d5e6ee9c54c
abo <abubker630a@gmail.com>
2023-12-31 15:14:37 +0500

    make AccountView shut up

--- Core/Account/AccountView.swift
+++ Core/Account/AccountView.swift
@@ -12,17 +12,60 @@ struct AccountView: View {
     @ObservedObject var vmUser = UserInfoViewModel()
     
     var body: some View {
-        ZStack {
-            Color.themeView.background.ignoresSafeArea(.all)
-            ScrollView {
-                List {
-                    
+        NavigationStack {
+            ZStack {
+                Color.themeView.background.ignoresSafeArea(.all)
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        
+                        NavigationLink {
+                            
+                        } label: {
+                            HStack(alignment: .center,spacing: 10) {
+                                ZStack {
+                                    if let image = vmUser.imageProfiles {
+                                        Image(uiImage: image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 80,height: 80)
+                                            .background(Color.theme.Gray03)
+                                            .clipShape(Circle())
+                                    } else if vmUser.imageProfiles == nil {
+                                        Image("")
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 80,height: 80)
+                                            .background(Color.theme.Gray03)
+                                            .clipShape(Circle())
+                                    }
+                                }
+                                
+                                Text(vmUser.UserName)
+                                    .font(.system(size: 18,weight: .regular))
+                                    .foregroundStyle(Color.themeView.secondaryText)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                            }.padding(.all)
+                                .background(Color.theme.Gray06)
+                                .clipShape(.rect(cornerRadius: 10))
+                        }
+
+                 
+                   
+                       
+                    }.padding(.all)
                 }
             }
+            .navigationTitle("Account")
+            
         }
     }
 }
 
-#Preview {
-    AccountView()
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+            .preferredColorScheme(.dark)
+    }
 }
--- Onboarding/OnboardingView.swift
+++ Onboarding/OnboardingView.swift
@@ -44,7 +44,6 @@ struct OnboardingView: View {
                                 .foregroundStyle(Color.white)
                         }
                     }
-                        
                 }
             }
          
